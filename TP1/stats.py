64cefd79cff7019afe594fbe1dc9d1c412c9b27e
hgp <henriquepereira31337@gmail.com>
2024-02-13 16:19:47 +0000

    feat: add tp1

---
+++ TP1/stats.py
@@ -0,0 +1,41 @@
+entradas = 0
+modalidades = []
+aptos = 0
+idades = []
+
+f = open("emd.csv", "r")
+
+f.readline() # ignorar a primeira linha
+
+for line in f:
+    entradas += 1
+    item = line.split(',')
+    idades.append(int(item[5]))
+    
+    if item[11] == "true":
+        aptos += 1
+        
+    if item[8] not in modalidades:
+        modalidades.append(item[8])
+f.close()
+
+modalidades.sort()
+
+aptos = (aptos/entradas)*100
+n_aptos = 100 - aptos
+
+faixa_etaria = [0] * 21
+for i in idades:
+    faixa_etaria[int(idades[i]/5)] += 1
+
+##
+print("--Resultados--", '\n',
+      "Lista Ordenada de Modalidades:", modalidades, '\n',
+      "Percentagem de atletas aptos e inaptos:", aptos, n_aptos, '\n'
+      "Distribuicao por Faixa etarias:")
+i = 0
+while i < len(faixa_etaria):
+    a = i * 5
+    print(f'[{a}-{a+5}] = {faixa_etaria[i]}')
+    i += 1
+##  
